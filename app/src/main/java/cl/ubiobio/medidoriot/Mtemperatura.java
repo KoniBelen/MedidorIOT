41c2fa684eee3c9a0cbe6353088c10ec40bd05e5
matias chavez <matroxmega@gmail.com>
2019-04-23 20:05:39 -0400

    se agrego boton flotante a gauge de pantalla de temeperatura

--- app/src/main/java/cl/ubiobio/medidoriot/Mtemperatura.java
+++ app/src/main/java/cl/ubiobio/medidoriot/Mtemperatura.java
@@ -1,6 +1,8 @@
 package cl.ubiobio.medidoriot;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +17,15 @@ public class Mtemperatura extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mtemperatura);
 
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
         volver = (Button)findViewById(R.id.vuelveM);
 
        volver.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +35,6 @@ public class Mtemperatura extends AppCompatActivity {
                startActivity(iralmenu);
            }
        });
+
     }
 }

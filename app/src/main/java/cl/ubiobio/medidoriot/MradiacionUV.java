ec5b28b0350d7ca3aefc871d4da94403d916c46f
matias chavez <matroxmega@gmail.com>
2019-04-24 17:15:39 -0400

    se agrego boton flotante a pantallas faltantes y se agrego gauge generico para pantallas faltantes

--- app/src/main/java/cl/ubiobio/medidoriot/MradiacionUV.java
+++ app/src/main/java/cl/ubiobio/medidoriot/MradiacionUV.java
@@ -1,6 +1,8 @@
 package cl.ubiobio.medidoriot;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +16,16 @@ public class MradiacionUV extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mradiacion_uv);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
 
         volver = (Button)findViewById(R.id.almenu);
 

038a8851ef80290914d9d8353b3740954acbe911
matias chavez <matroxmega@gmail.com>
2019-04-26 18:23:15 -0400

    Se replico busqueda del valor para gaugue en clases de Mradiacion y Mtemperatura.

--- app/src/main/java/cl/ubiobio/medidoriot/Mtemperatura.java
+++ app/src/main/java/cl/ubiobio/medidoriot/Mtemperatura.java
@@ -8,21 +8,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Calendar;
+import java.util.Date;
+
 public class Mtemperatura extends AppCompatActivity {
 
     Button volver;
+    //creamos la consulta
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mtemperatura);
+        //inicializamos la consulta
+        queue = Volley.newRequestQueue(this);
+        SetValorGauge();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                SetValorGauge();
 
             }
         });
@@ -35,6 +53,42 @@ public class Mtemperatura extends AppCompatActivity {
                startActivity(iralmenu);
            }
        });
+    }
+    private void SetValorGauge(){
+        //buscamos la fecha actual
+        Calendar rightNow = Calendar.getInstance();
+        Date fechaActual = rightNow.getTime();
+        Integer dia = fechaActual.getDay();
+        Integer mes = fechaActual.getMonth();
+        Integer anio = fechaActual.getYear();
+        String fechaConcatenada = "" + dia + mes + anio;
+        //url de consulta
+        String url = "http://arrau.chillan.ubiobio.cl:8075/ubbiot/web/mediciones/medicionespordia/lWTXt6CeLP/8IvrZCP3qa/" + fechaConcatenada;
 
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                String valor;
+                try {
+                    JSONArray array = response.getJSONArray("data");
+                    for (int i =0; i<array.length();i++) {
+                        JSONObject object = array.getJSONObject(i);
+                        valor = object.getString("valor");
+                    }
+                    //falta setear valor al gaugue
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+
+            }
+        });
+        queue.add(request);
     }
 }
